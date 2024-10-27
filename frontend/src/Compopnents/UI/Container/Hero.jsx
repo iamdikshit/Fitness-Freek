a0192aaf99231750922ff351311af1c8dc77f27d
Dikshit Bhardwaj <54199681+iamdikshit@users.noreply.github.com>
2024-10-27 08:50:36 +0530

    Update Hero.jsx

--- frontend/src/Compopnents/UI/Container/Hero.jsx
+++ frontend/src/Compopnents/UI/Container/Hero.jsx
@@ -10,7 +10,7 @@ const Hero = () => {
       transition={SectionAnimate.transition}
       className="hero-section md:pb-24 bg-white overflow-x-hidden"
     >
-      <div className="relative flex flex-col md:flex-row items-center justify-end">
+      <div className="relative mx-auto flex flex-col md:flex-row items-center justify-end">
         <div className="py-4 px-8 md:px-16">
           <div className="flex flex-col mt-12 md:mt-24 w-3/4 sm:w-1/2 gap-4 lg:gap-6">
             <h1 className="py-2 font-roboto text-3xl sm:text-4xl lg:text-6xl font-bold relative after:h-[4px] after:w-[30%] after:bg-red-700 after:absolute after:bottom-0 after:left-0 after:transition-all after:duration-300 after:ease-in-out">
